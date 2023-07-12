fa0e545596b295b575dba434055e234fdbd7d0d1
vaishnavirtp <vaishnavi.ranbhare@techprescient.com>
2023-07-12 15:18:11 +0530

    Updated the code with recent changes

--- TODO Api/app/routes/router.py
+++ TODO Api/app/routes/router.py
@@ -1,23 +1,14 @@
 from fastapi import FastAPI, HTTPException, Depends, APIRouter
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 from uuid import UUID
 import app.models.models as models
-from app.database.database import engine, Sessionlocal
+from app.database.database import get_db
 from sqlalchemy.orm import Session
-from app.schemas.schemas import Task, TaskDetails
+from app.schemas.schemas import Task, TaskDetails, Priority
 from sqlalchemy import desc
 
-
-models.Base.metadata.create_all(bind=engine)
-
 router = APIRouter()
-
-
-def get_db():
-    try:
-        db = Sessionlocal()
-        yield db
-    finally:
-        db.close()
 
 
 # ------------------------------ Get Requests------------------------------
@@ -27,7 +18,7 @@ def get_tasks(db: Session = Depends(get_db)):
 
 
 @router.get("/priority-tasks/{priority}")
-def get_tasks(priority: str, db: Session = Depends(get_db)):
+def get_tasks(priority: Priority, db: Session = Depends(get_db)):
     return db.query(models.Tasks).filter(models.Tasks.priority == priority).all()
 
 
@@ -44,56 +35,60 @@ def get_tasks(db: Session = Depends(get_db)):
 # ------------------------------ Post Requests------------------------------
 
 
-@router.post("/")
-def create_task(task: Task, db: Session = Depends(get_db)):
-    task_model = models.Tasks()
-    task_model.name = task.name
-    task_model.description = task.description
-    task_model.priority = task.priority
-    task_model.start_date = task.start_date
-    task_model.end_date = task.end_date
-    task_model.status = task.status
+@router.post("/", status_code=201)
+async def create_task(task: Task, db: Session = Depends(get_db)):
+    try:
+        task_model = models.Tasks(**task.dict())
 
-    db.add(task_model)
-    db.commit()
-    return HTTPException(status_code=201, detail=f"Successfully created the task")
+        db.add(task_model)
+        db.commit()
+        return JSONResponse(content="Successfully created the task")
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=400, detail=f"Some error occured.")
 
 
 # ------------------------------ Put Requests------------------------------
 
 
-@router.put("/{task_id}")
-def update_task(task_id: int, task: Task, db: Session = Depends(get_db)):
-    task_model = db.query(models.Tasks).filter(models.Tasks.id == task_id).first()
-    if task_model is None:
-        raise HTTPException(status_code=404, detail="ID doesn't exist")
-    task_model.name = task.name
-    task_model.description = task.description
-    task_model.priority = task.priority
-    task_model.start_date = task.start_date
-    task_model.end_date = task.end_date
-    task_model.status = task.status
+@router.put("/{task_id}", status_code=200)
+async def update_task(task_id: int, task: TaskDetails, db: Session = Depends(get_db)):
+    try:
+        task_model = db.query(models.Tasks).filter(models.Tasks.id == task_id).first()
+        if task_model is None:
+            raise HTTPException(status_code=404, detail="ID doesn't exist")
+        task_model.name = task.name
+        task_model.description = task.description
+        task_model.priority = task.priority
 
-    db.add(task_model)
-    db.commit()
-    return HTTPException(status_code=200, detail="Successfully updated the task")
+        db.add(task_model)
+        db.commit()
+        return JSONResponse(content="Successfully updated the task")
+    except:
+        return HTTPException(status_code=404, detail=f"Task Not found.")
 
 
 # ------------------------------ Delete Requests------------------------------
 
 
-@router.delete("/{task_id}")
-def delete_task(task_id: int, db: Session = Depends(get_db)):
-    task_model = db.query(models.Tasks).filter(models.Tasks.id == task_id).first()
-    if task_model is None:
-        raise HTTPException(status_code=404, detail="ID doesn't exist")
-    db.query(models.Tasks).filter(models.Tasks.id == task_id).delete()
-    db.commit()
-    return HTTPException(status_code=200, detail="Successfully deleted the task")
+@router.delete("/{task_id}", status_code=200)
+async def delete_task(task_id: int, db: Session = Depends(get_db)):
+    try:
+        task_model = db.query(models.Tasks).filter(models.Tasks.id == task_id).first()
+        if task_model is None:
+            raise HTTPException(status_code=404, detail="ID doesn't exist")
+        db.query(models.Tasks).filter(models.Tasks.id == task_id).delete()
+        db.commit()
+        return JSONResponse(content="Successfully deleted the task")
+    except:
+        return HTTPException(status_code=404, detail=f"Task Not found.")
 
 
-@router.delete("/delete-all/")
-def delete_task(db: Session = Depends(get_db)):
-    db.query(models.Tasks).delete()
-    db.commit()
-    return HTTPException(status_code=200, detail="Successfully deleted all the task")
+@router.delete("/delete-all/", status_code=200)
+async def delete_task(db: Session = Depends(get_db)):
+    try:
+        db.query(models.Tasks).delete()
+        db.commit()
+        return JSONResponse(content="Successfully deleted all the tasks")
+    except:
+        return HTTPException(status_code=400, detail=f"Some error occured.")
--- TODO Api/app/schemas/schemas.py
+++ TODO Api/app/schemas/schemas.py
@@ -1,6 +1,13 @@
 from typing import Optional
 from pydantic import BaseModel, Field
 from datetime import date
+from enum import Enum
+
+
+class Priority(str, Enum):
+    high = "high"
+    medium = "medium"
+    low = "low"
 
 
 class Task(BaseModel):
@@ -9,7 +16,7 @@ class Task(BaseModel):
         min_length=1,
         max_length=255,
     )
-    priority: str = Field(min_length=1, max_length=20)
+    priority: str = Priority
     start_date: date
     end_date: date
     status: bool = Field(default=False)
@@ -18,3 +25,4 @@ class Task(BaseModel):
 class TaskDetails(BaseModel):
     name: str = Field(min_length=1)
     description: str = Optional[str]
+    priority: str = Optional[str]
